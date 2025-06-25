98ad9e59b0f87631ebeb8577d07ee87b289d0c76
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-06-25 11:16:33 +0000

    fix: add API rewrites to next.config.ts for backend service routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://listapro-backend-prod-service:8080/api/:path*'
+      }
+    ];
+  },
 };
 
 export default nextConfig;
